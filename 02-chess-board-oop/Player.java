d9b8375366d9a1bb751b49c5f67c20d706a02236
Arianna Siniscalchi <siniscalchi.arianna@gmail.com>
2026-05-24 21:16:06 +0200

    Add Player class with color attribute and methods

---
+++ 02-chess-board-oop/Player.java
@@ -0,0 +1,18 @@
+package scacchiera;
+
+import scacchiera.Color;
+
+public class Player {
+
+	private final Color color;
+	public Player(Color color){
+	this.color=color;
+	}
+	@Override
+	public String toString() {
+	return "Player [color=" + color + "]";
+	}
+	public Color getColor() {
+	return color;
+	}
+}
