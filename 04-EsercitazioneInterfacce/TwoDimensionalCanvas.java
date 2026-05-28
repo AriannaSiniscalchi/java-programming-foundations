8a0f0704e0e8399c9435402f9786063b4e5420f9
Arianna Siniscalchi <siniscalchi.arianna@gmail.com>
2026-05-28 12:07:41 +0200

    Create TwoDimensionalCanvas.java

---
+++ 04-EsercitazioneInterfacce/TwoDimensionalCanvas.java
@@ -0,0 +1,10 @@
+package interfacce;
+
+public interface TwoDimensionalCanvas {
+
+	void drawPoint(Point point);
+	
+	void drawLine(Point pointa, Point pointb);
+	
+	void printCircle(Point center, double radius);
+}
